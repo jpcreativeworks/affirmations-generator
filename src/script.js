8679be3e729e720b1ec0279303b2df2217cc33d8
Jenn <jp.graphics.011@gmail.com>
2025-03-11 17:50:26 -0700

    added basic form handling and typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,17 @@
-alert("you are amazing");
+function generateAffirmation(event) {
+  event.preventDefault();
+  //let affirmationQuote = document.querySelector("#yourAffirmation");
+  new Typewriter("#yourAffirmation", {
+    strings: [
+      "I am on a journey of discovery, finding clarity with each step forward.",
+    ],
+    autoStart: true,
+    cursor: "",
+    loop: false,
+    delay: 75,
+  });
+  affirmationQuote.innerHTML = "";
+}
+
+let affirmationForm = document.querySelector("#affirmation-form");
+affirmationForm.addEventListener("submit", generateAffirmation);
